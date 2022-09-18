39f78582444af4e7a909ea836004ba8a34e8560b
Cristopher Alvear Candia <caalvearc@achs.cl>
2022-09-17 21:24:02 -0300

    chore: as.d.t

--- src/app/assets/assets.d.ts
+++ src/app/assets/assets.d.ts
@@ -1,13 +1,3 @@
-declare module '*.module.css' {
-    const classes: { [key: string]: string };
-    export default classes;
-}
-
-declare module '*.module.scss' {
-    const classes: { [key: string]: string };
-    export default classes;
-}
-
 declare module '*.svg' {
     import React = require('react');
     export const ReactComponent: React.FunctionComponent<
@@ -16,24 +6,4 @@ declare module '*.svg' {
 
     const src: string;
     export default src;
-}
-
-declare module '*.jpg' {
-    const content: string;
-    export default content;
-}
-
-declare module '*.png' {
-    const content: string;
-    export default content;
-}
-
-declare module '*.bmp' {
-    const content: string;
-    export default content;
-}
-
-declare module '*.json' {
-    const content: string;
-    export default content;
 }
