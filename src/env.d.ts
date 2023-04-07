fb834e68676a7ee331ecef92b64cb1ec61b0d688
Cristopher Alvear <calvear93@gmail.com>
2023-04-07 13:14:31 -0400

    chore: ✨ built-in types tweaks

--- src/env.d.ts
+++ src/env.d.ts
@@ -27,6 +27,26 @@ interface ImportMetaEnv {
 }
 
 declare global {
+	// SECTION: built-in types tweaks
+	interface JSON {
+		parse<T = unknown>(
+			text: string,
+			reviver?: (this: any, key: string, value: any) => any,
+		): T;
+	}
+
+	type ObjectKeys<T> = T extends object
+		? (keyof T)[]
+		: T extends number
+		? []
+		: T extends any[] | string
+		? string[]
+		: never;
+
+	interface ObjectConstructor {
+		keys<T>(o: T): ObjectKeys<T>;
+	}
+
 	// SECTION: global custom types
 	type decimal = number;
 
